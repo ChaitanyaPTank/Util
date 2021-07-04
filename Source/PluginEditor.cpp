d3b28a9b1bde17ec55b85b6c88e4daffea18339e
ChaitanyaPTank <tankchaitanya333@gmail.com>
2021-07-04 14:41:34 +0530

    init commit

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,53 @@
+/*
+  ==============================================================================
+
+	This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+UtilAudioProcessorEditor::UtilAudioProcessorEditor(UtilAudioProcessor& p)
+	: AudioProcessorEditor(&p), audioProcessor(p)
+{
+	// Make sure that before the constructor has finished, you've set the
+	// editor's size to whatever you need it to be.
+	setSize(300, 300);
+	mGainSlider.setBounds(100, 100, 100, 100);
+	mGainSlider.setSliderStyle(juce::Slider::RotaryVerticalDrag);
+	mGainSlider.setRange(0.0, 1.0, 0.01);
+	mGainSlider.setTextBoxStyle(juce::Slider::NoTextBox, false, 360, 0);
+	mGainSlider.setTextValueSuffix("Gain");
+	mGainSlider.setValue(1.0);
+
+	addAndMakeVisible(&mGainSlider);
+	mGainSlider.addListener(this);
+}
+
+UtilAudioProcessorEditor::~UtilAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void UtilAudioProcessorEditor::paint(juce::Graphics& g)
+{
+	// (Our component is opaque, so we must completely fill the background with a solid colour)
+	g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+
+	g.setColour(juce::Colours::white);
+	g.setFont(15.0f);
+	//g.drawFittedText("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void UtilAudioProcessorEditor::resized()
+{
+	// This is generally where you'll want to lay out the positions of any
+	// subcomponents in your editor..
+}
+
+void UtilAudioProcessorEditor::sliderValueChanged(juce::Slider* slider) {
+	audioProcessor.mGainFloat = mGainSlider.getValue();
+}
---
+++ Source/PluginEditor.h
@@ -0,0 +1,38 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class UtilAudioProcessorEditor  : public juce::AudioProcessorEditor,
+    private juce::Slider::Listener
+{
+public:
+    UtilAudioProcessorEditor (UtilAudioProcessor&);
+    ~UtilAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    UtilAudioProcessor& audioProcessor;
+    juce::Slider mGainSlider;
+
+    // callback function
+    void sliderValueChanged(juce::Slider* slider) override;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (UtilAudioProcessorEditor)
+};
